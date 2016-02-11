1e97ab9d6dc1bb4f2c4afe81e0947030337fa1ba
igor-suhorukov <igor.suhorukov@gmail.com>
2016-02-12 02:21:22 +0300

    fix classloading issue with camel core

--- src/main/java/com/github/igorsuhorukov/url/handler/camel/classloader/ClassLoaderUtils.java
+++ src/main/java/com/github/igorsuhorukov/url/handler/camel/classloader/ClassLoaderUtils.java
@@ -1,6 +1,7 @@
 package com.github.igorsuhorukov.url.handler.camel.classloader;
 
 import com.github.igorsuhorukov.smreed.dropship.MavenClassLoader;
+import org.apache.camel.CamelContext;
 
 import java.net.URL;
 import java.util.List;
@@ -10,7 +11,9 @@ public class ClassLoaderUtils {
     public static ClassLoader loadModule(String moduleName) throws Exception {
         ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
         List<URL> artifactUrlsCollection = MavenClassLoader.usingCentralRepo().getArtifactUrlsCollection(moduleName, null);
-        Thread.currentThread().setContextClassLoader(new ModuleClassLoader(artifactUrlsCollection.toArray(new URL[artifactUrlsCollection.size()]), contextClassLoader));
+        URL[] classpath = artifactUrlsCollection.toArray(new URL[artifactUrlsCollection.size()]);
+        ClassLoader camelCoreClassLoader = CamelContext.class.getClassLoader();
+        Thread.currentThread().setContextClassLoader(new ModuleClassLoader(classpath, camelCoreClassLoader));
         return contextClassLoader;
     }
 
